c40be6af77077df07880223550ef3df03822767b
Francis Bourgouin <netbeuix@gmail.com>
2020-02-04 19:48:02 -0500

    changed to css variables + easter egg

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+
 import "./App.scss";
 import { ComponentForm } from "./components/ComponentForm";
 import { ComponentList } from "./components/ComponentList";
@@ -9,16 +10,27 @@ import {
   emptyLocalStorage
 } from "./helpers";
 import { ComponentsContext, VariablesContext } from "./hooks/contextHooks";
+import { CompactPicker } from "react-color";
 
 const App = () => {
   const [components, setComponents] = useState(exampleComponentMap);
   const [variables, setVariables] = useState(exampleVariablesList);
   const [easterEgg, setEasterEgg] = useState(false);
+  const [backgroundValue, setBackgroundValue] = useState(null);
 
   useEffect(() => {
     getLocalStorage(setComponents);
   }, []);
 
+  const setBackground = color => {
+    const { r, g, b } = color.rgb;
+    const colorValue = `rgba(${r}, ${g}, ${b}, 0.2)`;
+    document.documentElement.style.setProperty(
+      "--background-color",
+      colorValue
+    );
+    setBackgroundValue(color);
+  };
   return (
     <ComponentsContext.Provider value={{ components, setComponents }}>
       <VariablesContext.Provider value={{ variables, setVariables }}>
@@ -26,7 +38,16 @@ const App = () => {
           <h1 onDoubleClick={() => setEasterEgg(!easterEgg)}>
             React component mapper
           </h1>
-          {easterEgg && <input type='color'></input>}
+          {easterEgg && (
+            <header>
+              <h2>Choose a new color !</h2>
+              <CompactPicker
+                color={backgroundValue ? backgroundValue.rgb : ""}
+                onChange={setBackground}
+                onChangeComplete={setBackground}
+              />
+            </header>
+          )}
           <ComponentForm />
           <section className='componentList'>
             <ComponentList current={components["App"]} availableProps={[]} />
