cf44c77796b8a6d57349cb9a7745bccbd57d8b45
Francis Bourgouin <netbeuix@gmail.com>
2020-02-14 23:04:56 -0500

    Added download functionnality + variable storage

--- src/helpers.js
+++ src/helpers.js
@@ -1,15 +1,39 @@
-export const storeInLocalStorage = components => {
+export const storeLSComponents = components => {
   localStorage.setItem("componentMap", JSON.stringify(components));
 };
-export const emptyLocalStorage = () => {
-  localStorage.setItem("componentMap", "");
-};
 
-export const getLocalStorage = callback => {
+export const getLSComponents = callback => {
   if (localStorage.getItem("componentMap")) {
     const cMap = JSON.parse(localStorage.getItem("componentMap"));
     if (cMap) {
       callback(cMap);
     }
   }
+};
+
+export const storeLSVariables= variables => {
+  localStorage.setItem("variableMap", JSON.stringify(variables));
+};
+
+export const getLSVariables = callback => {
+  if (localStorage.getItem("variableMap")) {
+    const vMap = JSON.parse(localStorage.getItem("variabletMap"));
+    if (vMap) {
+      callback(vMap);
+    }
+  }
+};
+
+export const componentJSONLink = components => {
+  const output = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(components));
+  return `data:${output}`
+}
+export const variableJSONLink = variables => {
+  const output = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(variables));
+  return `data:${output}`
+}
+
+export const emptyLocalStorage = () => {
+  localStorage.setItem("componentMap", "");
+  localStorage.setItem("variableMap", "");
 };
